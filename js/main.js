0d81df87b99ccc11ea7b9f0d074443e4ccebb8e9
Timkoo <bfg20k@mail.ru>
2022-02-20 14:14:20 +0300

    Добавлена ссылка на main.js. Доработаны функции getRandomOnInterval, checkStingLength в main.js. Повторно добавлен куратор.

--- js/main.js
+++ js/main.js
@@ -1,17 +1,22 @@
-// подсмотрено на https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/round
+// подсмотрено на https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
 function getRandomOnInterval (num1, num2) {
-  num1 = Math.round(num1);
-  num2 = Math.round(num2);
-  return Math.floor(Math.random()*(num2 - num1 + 1) + num1);
+/*  num1 = Math.round(num1);
+  num2 = Math.round(num2);*/
+  if (num1 >= num2) {
+    const num3 = num1;
+    num1 = num2;
+    num2 = num3;
+  }
+  return num1 === num2 ? num1 : Math.floor(Math.random()*(num2 - num1 + 1)) + num1;
 }
 
 // eslint-disable-next-line no-console
-console.log(getRandomOnInterval(3.49,7.5));
+console.log(getRandomOnInterval(4,4));
 
 function checkStingLength (string, maxLength) {
-  const stringLength = string.toString().length;
-  return (stringLength <= maxLength);
+  /*const stringLength = string.toString().length;*/
+  return (string.length <= maxLength);
 }
 
 // eslint-disable-next-line no-console
-console.log(checkStingLength(3551234, 6));
+console.log(checkStingLength('3551234', 6));
