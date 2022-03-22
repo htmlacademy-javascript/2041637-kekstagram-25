ecff59170966ab4c9d548f532cd7c992ad680b2a
Timkoo <bfg20k@mail.ru>
2022-03-22 22:43:30 +0300

    Доработаны функции

--- js/image-upload.js
+++ js/image-upload.js
@@ -19,6 +19,32 @@ const imageUploadHashtags = imageUploadForm.querySelector('.text__hashtags');
 const imageUploadDescription = imageUploadForm.querySelector('.text__description');
 const pristine = new Pristine(imageUploadForm, pristineConfig);
 const reg = RegExp(`^#[a-zA-Z]{${HASHTAG_MINLENGTH},${HASHTAG_MAXLENGTH}}$`);
+const smallerButton = imageUploadForm.querySelector('.scale__control--smaller');
+const biggerButton = imageUploadForm.querySelector('.scale__control--bigger');
+const scaleSizeField = imageUploadForm.querySelector('.scale__control--value');
+const image = imageUploadForm.querySelector('.img-upload__preview').querySelector('img');
+const noneEffect = imageUploadForm.querySelector('.effects__radio[value=none]');
+const effectsList = imageUploadForm.querySelector('.effects__list');
+const sliderDiv = imageUploadForm.querySelector('.effect-level__slider');
+const effectValue = imageUploadForm.querySelector('.effect-level__value');
+
+let filter;
+const filterName = {
+  chrome: 'grayscale',
+  sepia: 'sepia',
+  marvin: 'invert',
+  phobos: 'blue',
+  heat: 'brightness',
+};
+
+noUiSlider.create(sliderDiv, {
+  start: [1],
+  range: {
+    'min' : [0],
+    'max' : [1],
+  },
+  step: 0.1,
+});
 
 const imageUploadFormCloseHandler = (evt) => {
   if (isEscPressed(evt) || evt.type === 'click') {
@@ -30,13 +56,103 @@ const imageUploadFormCloseHandler = (evt) => {
   }
 };
 
+effectsList.addEventListener('change', (evt) => {
+  if (evt.target.closest('.effects__radio')) {
+    image.classList = '';
+    sliderDiv.noUiSlider.reset();
+    image.style.filter = 'none';
+    effectValue.value = '';
+    image.classList.add(`effects__preview--${evt.target.value}`);
+    filter = evt.target.value;
+    image.setAttribute('style', `filter:${filterName[filter]}(${sliderDiv.noUiSlider.get()})`);
+    evt.target.value === 'none' ? sliderDiv.classList.add('hidden') : sliderDiv.classList.remove('hidden');
+    if (evt.target.value === 'chrome' || evt.target.value === 'sepia') {
+      sliderDiv.noUiSlider.updateOptions({
+        range: {
+          'min': [0],
+          'max': [1],
+        },
+        step: 0.1,
+        start: 1,
+      });
+    }
+    if (evt.target.value === 'marvin') {
+      sliderDiv.noUiSlider.updateOptions({
+        range: {
+          'min': [0],
+          'max': [100],
+        },
+        step: 1,
+        start: 100,
+        format: {
+          to: function (value) {
+            return `${value}%`;
+          }
+        }
+      });
+    }
+    if (evt.target.value === 'phobos') {
+      sliderDiv.noUiSlider.updateOptions({
+        range: {
+          'min': [0],
+          'max': [3],
+        },
+        step: 0.1,
+        start: 3,
+        format: {
+          to: function (value) {
+            return `${value}px`;
+          }
+        }
+      });
+    }
+    if (evt.target.value === 'heat') {
+      sliderDiv.noUiSlider.updateOptions({
+        range: {
+          'min': [1],
+          'max': [3],
+        },
+        start: 3,
+        step: 0.1,
+      });
+    }
+  }
+});
+
+sliderDiv.noUiSlider.on('change', () => {
+  effectValue.value = sliderDiv.noUiSlider.get();
+  image.setAttribute('style', `filter:${filterName[filter]}(${sliderDiv.noUiSlider.get()})`);
+});
+
 imageUploadInput.addEventListener('change', (evt) => {
   if (evt.target.value) {
+    scaleSizeField.value = '100%';
     imageUploadOverlay.classList.remove('hidden');
     document.body.classList.add('modal-open');
+    if (noneEffect.checked) {
+      sliderDiv.classList.add('hidden');
+    }
     document.addEventListener('keydown', imageUploadFormCloseHandler);
     imageUploadCancelButton.addEventListener('click', imageUploadFormCloseHandler);
   }
+});
+
+smallerButton.addEventListener('click', () => {
+  let imageSizeValue = +scaleSizeField.value.slice(0, -1);
+  if (imageSizeValue > 25) {
+    imageSizeValue -= 25;
+  }
+  scaleSizeField.value = `${imageSizeValue}%`;
+  image.style.transform = `scale(${imageSizeValue/100})`;
+});
+
+biggerButton.addEventListener('click', () => {
+  let imageSizeValue = +scaleSizeField.value.slice(0, -1);
+  if (imageSizeValue < 100) {
+    imageSizeValue += 25;
+  }
+  scaleSizeField.value = `${imageSizeValue}%`;
+  image.style.transform = `scale(${imageSizeValue/100})`;
 });
 
 pristine.addValidator(imageUploadHashtags, (value) => {
@@ -92,5 +208,3 @@ imageUploadForm.addEventListener('submit', (evt) => {
     console.log('Форма невалидна');
   }
 });
-
-
