2d44a65bfa4fe0ae30490b2e65fb25e6cd959aa0
Keks <keks@htmlacademy.ru>
2022-04-05 21:22:56 +0300

    :heavy_check_mark: Сборка #12

--- 12/js/main.js
+++ 12/js/main.js
@@ -1,11 +1,14 @@
-import {renderImagesFromData} from './image-render.js';
+import {clearRenderedImages, renderImagesFromData} from './image-render.js';
 import {getData} from './data.js';
 import {showAlert} from './util.js';
-import {addFilterListeners, applyDefaultFilter, showFilterMenu} from './filter.js';
+import {applyFilter, addFilterListeners, FilterType, showFilterMenu} from './filter.js';
 import './image-upload.js';
 
 getData((imageDescriptions) => {
   showFilterMenu();
-  applyDefaultFilter(imageDescriptions, () => renderImagesFromData(imageDescriptions));
-  addFilterListeners(imageDescriptions, renderImagesFromData);
+  applyFilter(FilterType.DEFAULT, imageDescriptions, renderImagesFromData);
+  addFilterListeners(imageDescriptions, (filteredImageDescriptions) => {
+    clearRenderedImages();
+    renderImagesFromData(filteredImageDescriptions);
+  });
 }, showAlert);
